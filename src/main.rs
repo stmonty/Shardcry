23acd74a75300396b93b9d08b9fa46c725e6dddd
stmonty <stmontydev@gmail.com>
2022-01-14 01:28:18 -0600

    Worked on map and graphics and camera

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 mod map;
+mod map_builder;
 mod player;
+mod camera;
 
 mod prelude {
 
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
     pub use crate::player::*;
+    pub use crate::map_builder::*;
+    pub use crate::camera::*;
 }
 
 use prelude::*;
@@ -15,31 +21,43 @@ use prelude::*;
 struct State{
     map: Map,
     player: Player,
+    camera: Camera,
 }
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new(),
-            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm){
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
 
 }
 
 fn main() -> BError{
-    let context= BTermBuilder::simple80x50()
+    let context= BTermBuilder::new()
         .with_title("Shardcry")
         .with_fps_cap(30.0)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
