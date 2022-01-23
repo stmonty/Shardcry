cac809182748d0e3e424b3a422267d8fc8b6556e
stmonty <stmontydev@gmail.com>
2022-01-22 22:46:21 -0600

    Working on map-render system

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ impl GameState for State {
         ctx.cls();
         self.resources.insert(ctx.key);
         self.systems.execute(&mut self.ecs, &mut self.resources);
-        // TODO Render draw buffer
+        render_draw_buffer(ctx).expect("Render error");
     }
 
 }
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,5 +1,8 @@
 use crate::prelude::*;
 
 pub fn build_scheduler() -> Schedule {
-    Schedule::builder().build()
+    Schedule::builder()
+        .add_system(player_input::player_input_system())
+        .add_system(map_render::map_render_system())
+        .build()
 }
